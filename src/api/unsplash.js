46ba6c479aff44574cd3d60fe9c25e4666d2f895
Raşid Ağaç <rasitagac@hotmail.com>
2022-09-02 01:14:25 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
 	baseURL: "https://api.unsplash.com",
 	headers: {
-		Authorization: "Client-ID nWQ6zhQSb1g1LmgEZgu9hG27SrnXFfxmeiI7X_eBnMM",
+		Authorization: "Client-ID UNSPLASH_API_KEY",
 	},
 });
